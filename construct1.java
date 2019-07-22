15b6d4a04fd1ca38e1411728dce7c6e62b31c152
Samyak Gaur <samyakgaur@yahoo.com>
2019-07-22 10:35:48 +0530

    Add files via upload
    session 2

--- construct1.java
+++ construct1.java
@@ -15,18 +15,30 @@ class construct1
 	//making a custom constructor
 	construct1(){
 	a=1;
-	// use f to initialize float or else it takes it as double by default
 	f=5.0f;
 	d=10.0;
 	s="abc";
 	}
+	//making a parameterized constructor 
+	construct1(int a1,float f1, double d1 , String s1){
+	a=a1;
+	f=f1;
+	d=d1;
+	s=s1;
+	}
 	public static void main(String args[])
 		{
 			construct1 ob = new construct1();
+			construct1 obj = new construct1(19,20.0f,30.0,"sam");
 			System.out.println("value of a : "+ob.a);
 			System.out.println("value of f : "+ob.f);
 			System.out.println("value of d : "+ob.d);
-			System.out.println("value of s : "+ob.s);		
+			System.out.println("value of s : "+ob.s);
+			//Values by parameterized constructor 
+			System.out.println("value of a : "+obj.a);
+			System.out.println("value of f : "+obj.f);
+			System.out.println("value of d : "+obj.d);
+			System.out.println("value of s : "+obj.s);		
 		}	//End of main	
 
 }	//End of class cmdinput
@@ -48,5 +60,17 @@ value of a : 1
 value of f : 0.0
 value of d : 10.0
 value of s : abc
+
+MAKING A PARAMETERIZED CONSTRUCTOR 
+universe@dell15:~/Desktop/8604$ javac construct1.java
+universe@dell15:~/Desktop/8604$ java construct1
+value of a : 1
+value of f : 5.0
+value of d : 10.0
+value of s : abc
+value of a : 19
+value of f : 20.0
+value of d : 30.0
+value of s : sam
 
 */ 
