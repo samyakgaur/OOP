7ce30cdfc1b06959a934ea9da6f17d6e5ea386b4
Samyak Gaur <samyakgaur@yahoo.com>
2019-07-22 10:20:13 +0530

    Add files via upload
    session 2

---
+++ construct1.java
@@ -0,0 +1,33 @@
+/* 
+Samyak Gaur(8604)
+Batch B
+
+A program to take input from command line and print it
+*/
+
+import java.util.*;
+class construct1
+{
+	int a;
+	float f;
+	double d;
+	public static void main(String args[])
+		{
+			construct1 ob = new construct1();
+			System.out.println("value of a : "+ob.a);
+			System.out.println("value of f : "+ob.f);
+			System.out.println("value of d : "+ob.d);		
+		}	//End of main	
+
+}	//End of class cmdinput
+
+
+/*
+Output is as follows :
+universe@dell15:~/Desktop/8604$ javac construct1.java
+universe@dell15:~/Desktop/8604$ java construct1
+value of a : 0
+value of f : 0.0
+value of d : 0.0
+
+*/ 
