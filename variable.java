a9129bf17d73eb1f234df0811ab9a5d3bb6da3a6
Samyak Gaur <samyakgaur@yahoo.com>
2019-07-23 23:05:53 +0530

    decreased the font size

--- variable.java
+++ variable.java
@@ -7,12 +7,12 @@ A program to display global,instance and local variable and study its scope
 import java.lang.*;
 class variable
 {
-	static int globalv = 100;						// global variable 
-	int instance = 200;								// instance variable 
+	static int globalv = 100;				// global variable 
+	int instance = 200;					// instance variable 
 	public static void main(String args[])
 	{
 		variable obj=new variable();
-		int localvar=10;							// local variable 
+		int localvar=10;				// local variable 
 		System.out.println("local variable: "+localvar); 	
 		System.out.println("instance variable: "+obj.instance);
 		System.out.println("global varibale: "+globalv);		
