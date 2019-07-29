512c6a1c63a3f718aa909c79c6611bc1fce11796
Samyak Gaur <samyakgaur@yahoo.com>
2019-07-29 10:44:32 +0530

    Add files via upload

--- matrix.java
+++ matrix.java
@@ -19,7 +19,7 @@ class matrix
 	}//End of parameterized constructor
 	void readmat(){
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the elements of the matrix");
+		System.out.print("Enter the elements of the matrix: ");
 		for(int i=0;i<this.rows;i++){
 			for(int j=0;j<this.cols;j++){
 				m[i][j]=sc.nextInt();
@@ -38,12 +38,46 @@ class matrix
 	public static void main(String args[])
 		{	
 			Scanner sc1 = new Scanner(System.in);
-			System.out.println("Enter the rows and columns for the matrix: ");
+			System.out.println("Enter the rows and columns for the matrix 1 : ");
 			int r1=sc1.nextInt();
 			int c1=sc1.nextInt();
 			matrix  m1= new matrix(r1,c1);
 			m1.readmat();
 			m1.display();	
+			System.out.println("Enter the rows and columns for the matrix 2 : ");
+			int r2=sc1.nextInt();
+			int c2=sc1.nextInt();
+			matrix  m2= new matrix(r2,c2);
+			m2.readmat();
+			m2.display();
+			while(true){
+				System.out.println("Following are the options to perform on matrix:");
+				System.out.println("\n 1.Input matrix A \n 2.Input matrix B \n 3.Display of Matrix \n 4.Symmetric  \n 5.Transpose \n 6.Exit");
+				int choice = sc1.nextInt();
+				switch(choice){
+					case 1:
+							
+							break;
+					case 2:
+							
+							break;
+					case 3:
+							
+							break;
+					case 4:
+							
+							break;
+					case 5:
+							
+							break;
+					case 6:
+							System.exit(0);
+							break;
+					default:
+							System.out.println("Invalid Choice");
+							break;					
+				}//End of switch case			
+			}//End of while loop	
 		}	//End of main	
 
 
